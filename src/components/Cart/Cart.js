20fc96c2e945593db325afaadd4d611d423308a5
Arturas Jurevicius <arturyyys@gmail.com>
2021-10-05 20:00:55 +0300

    v139.1 fix cart button classes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,7 +18,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div className={classes.action}>
+      <div className={classes.actions}>
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
