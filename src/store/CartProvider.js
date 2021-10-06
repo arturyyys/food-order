124bf0dc4962fe8259e0b3f7c78d9a32a51e055c
Arturas Jurevicius <arturyyys@gmail.com>
2021-10-06 20:15:32 +0300

    v142. Using the Context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CartContext from "./cart-contex";
+import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
   const addItemToCartHandler = (item) => {};
